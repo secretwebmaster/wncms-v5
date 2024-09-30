238d3b680939011c07b917c24b9b91355c45efc9
secretwebmaster <earnbyshare2016@gmail.com>
2024-10-01 04:54:02 +0800

    Add theme files

---
+++ lang/zh_TW/default.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'header' => '頁首',
+    'footer' => '頁腳',
+    'general' => '一般',
+    'homepage' => '主頁',
+    'posts' => '文章',
+    'pages' => '頁面',
+    'ads' => '廣告',
+    'site_info' => '網站資訊',
+    'custom_codes' => '自訂代碼',
+];
---
+++ lang/zh_TW/starter.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'header' => '頁首',
+    'footer' => '頁腳',
+    'general' => '一般',
+    'homepage' => '主頁',
+    'posts' => '文章',
+    'pages' => '頁面',
+    'ads' => '廣告',
+    'advertisement' => '廣告',
+    'site_info' => '網站資訊',
+    'custom_code' => '自訂代碼',
+];
